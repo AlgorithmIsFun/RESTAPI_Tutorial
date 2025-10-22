116c420adf7448f880207a6008b63e51951390e0
Abdullah <abdullah61599@gmail.com>
2025-10-22 19:12:50 -0400

    Flask Test Tutorial

--- FlaskTutorial.py
+++ FlaskTutorial.py
@@ -1,10 +1,57 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except Exception as e:
+            db.session.rollback()
+            return f"There was a problem. {e}"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_delete)
+        db.session.commit()
+        return redirect(url_for('index'))
+    except:
+        db.session.rollback()
+        return"There was a problem deleting that task."
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect(url_for('index'))
+        except:
+            db.session.rollback()
+            return f"There was a problem with updating. {request.form['content']}"
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == '__main__':
     app.run(debug=True)
